93db58aaae7d78b1bd72867b451d783986fafe72
goraxe <goraxe@goraxe-laptop.(none)>
2009-12-31 10:24:41 +0000

    fix sync_links bugs

--- bin/sync_links.sh
+++ bin/sync_links.sh
@@ -64,7 +64,8 @@ function die {
 function is_vcs {
 	dir=$1
 	echo "is_vcs called with ${dir}"
-	return [[ -e "${dir}/.svn" ]] || [[ -e "{$dir}/.git" ]]
+	[[ -e "${dir}/.svn" || -e "${dir}/.git" ]]
+	return 
 }
 
 function dir_update {
@@ -77,7 +78,9 @@ function dir_update {
 		is_vcs_location ${dir} ${vcs_location}
 		IS_VCS_LOCATION=$?
 		if [[ ${IS_VCS} &&  ${IS_VCS_LOCATION} ]]; then
-			${VCS_UPDATE_CMD} ${dir}
+			pushd ${dir}				
+			${VCS_UPDATE_CMD}
+			popd
 			return
 		else 
 			echo "existing ${dir} being backed up to ${dir}.bck" 
@@ -96,9 +99,11 @@ function vcs_create {
 function is_vcs_location {
 	dir=$1
 	vcs_location=$2
-	push ${dir}
-	repo=`$VCS_LOCATION_CMD`
+	pushd ${dir}
+	echo "VCS_LOCATION_CMD $VCS_LOCATION_CMD"
+	repo=`${VCS_LOCATION_CMD}|${VCS_LOCATION_CMD_GREP}|cut -d" " -f 4`
 	popd
+	echo "REPO = ${repo} vcs_location = ${vcs_location}"
 #	repo=`svn info $dir | grep "Repository Root:" | cut -d" " -f 3`
 	[[ ${vcs_location} == ${repo} ]]
 	return 
