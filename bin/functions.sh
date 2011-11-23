054b50435e0b0f4c355d76315e894b5fb7722eb8
gordon <goraxe@goraxe.me.uk>
2011-11-22 16:27:37 -0800

    extra functions for floor, ceiling, debug

--- bin/functions.sh
+++ bin/functions.sh
@@ -1,32 +1,61 @@
 
 function die {
-	msg=$@
-	dt=`date +'%x %X'`
-	echo "$0 $dt: $msg" >&2
-	exit -1
+    msg=$@
+    dt=`date +'%x %X'`
+    echo "$0 error $dt: $msg" >&2
+    exit -1
 }
 
 function safe_mkdir {
-	DIR=$@
+    DIR=$@
 
-	if [[ -e ${DIR} ]]; then
-		if [[ ! -d ${DIR} ]]; then
-			die "${DIR} is not a directory"
-		fi
-	else
-		say "creating ${DIR}"
-		if [[ $DRY_RUN > 0 ]]; then
-			return;
-		fi
-		mkdir ${DIR} || die "could not mkdir ${DIR}"
-	fi
+    if [[ -e ${DIR} ]]; then
+        if [[ ! -d ${DIR} ]]; then
+            die "${DIR} is not a directory"
+        fi
+    else
+        say "creating ${DIR}"
+        if [[ $DRY_RUN > 0 ]]; then
+            return;
+        fi
+        mkdir ${DIR} || die "could not mkdir ${DIR}"
+    fi
 }
 
 function say {
-	if [[ ${VERBOSE} > 0 ]]; then
-		msg=$@
-		dt=`date +'%x %X'`
-		echo "$0 $dt: $msg"
-	fi
+    if [[ ${VERBOSE} > 0 ]]; then
+        msg=$@
+        dt=`date +'%x %X'`
+        echo "$0 info $dt: $msg"
+    fi
 }
 
+function debug {
+    if [[ ${DEBUG} > 0 ]]; then
+        msg=$@
+        dt=`date +'%x %X'`
+        echo "$0 debug $dt: $msg"
+    fi
+}
+
+function repeat() {
+      eval "printf '${1}%.0s' {1..${2}}"
+}
+
+function ceiling() {
+    local float_in=$1
+    local ceil_val=${float_in/.*}
+    if [ $float_in != $ceil_val ]; then 
+        ceil_val=$((ceil_val+1))
+    fi
+    echo "$ceil_val"
+}
+
+function floor() {
+    local float_in=$1
+    local floor_val=${float_in/.*}
+    if [ $float_in != $floor_val ]; then 
+        floor_val=$((floor_val+1))
+    fi
+    echo "$floor_val"
+}
